05833ca775a6f8a9d0ef8147646e468af5b49de3
Tasha <atanasovska87@hotmail.com>
2020-10-16 16:06:26 +0200

    Done

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 # imports go here
 
 def main():
-   """Main entry"""
-   pass
+  with open('example.txt', 'r') as f:
+     f_contents = f.read()
+     # print(f_contents, end='')
+
+  pass
+
 
 if __name__ == '__main__':
-   main()
-   
+  main()
+
+
+
+
+
