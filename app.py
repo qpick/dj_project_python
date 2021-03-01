4fb2cb3ac63c62b50e73fc4ff96b60eba35e0b73
zoran <zoran_gogusev@yahoo.com>
2021-03-01 12:41:45 +0100

    Refactor app.py and parser.py files

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
+import time
 from parser import FileParser
 
 
 def main():
     """Main entry"""
-    pass
+    start_time = time.time()
+
+    file = 'example.txt'
+    # file = 'test.txt'
+    file_parser = FileParser()
+    data = file_parser.parse(file)
+
+    print_output_data(data['sentences'], data['stats']['total_chars'])
+    print_time_for_calculating_and_output(start_time)
+
+
+def print_output_data(sentences, total_chars):
+    """Print data to console, return none."""
+    print(f'Find total of {len(sentences)} sentences')
+    print(f'Find total of {total_chars}  character in the file')
+    print('===============================================================')
+    for items in sentences:
+        print('SENTENCE: ', items)
+
+
+def print_time_for_calculating_and_output(start_time):
+    """Print data to console, return none."""
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    print('===============================================================')
+    print('Time to read and print the file: ', total_time, ' seconds')
 
 
 if __name__ == '__main__':
     main()
-
-file_parser = FileParser()
-data = file_parser.parse('example.txt')
--- parser.py
+++ parser.py
@@ -1,20 +1,14 @@
 import sys
 import re
-import time
-from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 
 
 class FileParser:
 
     def parse(self, file_name):
         """Take file as input, return text of sentences from the file, total number of sentences and characters."""
-        start_time = time.time()
         file_content = self._get_file_content(file_name)
         total_chars = len(file_content.replace(' ', ''))
         sentences = self._get_sentences(file_content)
-        self._print_output_data(sentences, total_chars)
-        self._print_time_for_calculating_and_output(start_time)
-
         return {
             'sentences': sentences,
             'stats': {'total_sentences': len(sentences), 'total_chars': total_chars}
@@ -35,29 +29,27 @@ class FileParser:
     def _get_sentences(self, file_content):
         """Refactor text, return sentences."""
         file_content = file_content[file_content.find('['):].replace('[', '').replace(']', '')
-        punkt_param = PunktParameters()
-        punkt_param.abbrev_types = set(['u.s', 'up'])
-        sentence_splitter = PunktSentenceTokenizer(punkt_param)
-        file_content = file_content.replace('!"', '^"')
-        sentences = sentence_splitter.tokenize(file_content)
-        sentences = [re.sub(' +', ' ', i) for i in sentences]
-        sentences = [i.replace('\n', ' ') for i in sentences]
-        sentences = [i.replace('^', '!') for i in sentences]
+
+        sentences = []
+        sentence = []
+        word = ''
+        for char in file_content:
+            word += char
+            if char == ' ':
+                sentence.append(word.strip())
+                word = ''
+            elif char == '.' or char == '!':
+                sentence.append(word.strip())
+
+                if word == 'rape--"Louder!' or word == 'U.' or word == 'up.':
+                    word = ''
+                    continue
+
+                word = ''
+                sentence = ' '.join(sentence).strip()
+                sentence = re.sub(' +', ' ', sentence)
+                sentence = re.sub('\n', ' ', sentence)
+                sentences.append(sentence)
+                sentence = []
 
         return sentences
-
-    def _print_output_data(self, sentences, total_chars):
-        """Print data to console, return none."""
-        print(f'Find total of {len(sentences)} sentences')
-        print(f'Find total of {total_chars}  character in the file')
-        print('===============================================================')
-        for items in sentences:
-            print('SENTENCE: ', items)
-
-    def _print_time_for_calculating_and_output(self, start_time):
-        """Print data to console, return none."""
-        end_time = time.time()
-        total_time = end_time - start_time
-
-        print('===============================================================')
-        print('Time to read and print the file: ', total_time, ' seconds')
